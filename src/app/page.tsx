106d04bde4773e5e65982421a1073adb88b78959
franssantander <santanderfrancisbeam@gmail.com>
2026-02-17 20:31:11 +0800

    added shadcnui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import { Button } from "@shared/components/ui/button";
+
 
 const page = () => {
   return <div>
-    <h1>Settings</h1>
+    <h1 className="font-medium text-4xl">Settings</h1>
+    <Button size="xs">Submit</Button>
   </div>;
 };
 
